c4c7bc0466066981f6b3d46a6aa99fb60704a774
Rafli Bangsawan <raflibangsawans@gmail.com>
2023-09-24 08:14:03 +0700

    added /GET/albums/:id endpoint

--- main.go
+++ main.go
@@ -16,6 +16,7 @@ type album struct {
 func main() {
     router := gin.Default()
     router.GET("/albums", getAlbums)
+	router.GET("/albums/:id", getAlbumByID)
 	router.POST("/albums", postAlbums)
 
     router.Run("localhost:8080")
